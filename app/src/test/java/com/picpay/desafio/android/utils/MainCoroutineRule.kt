4a8427bf0f478207dad7498b678ce4d1e4258aec
Filipedias0 <50221162+Filipedias0@users.noreply.github.com>
2022-09-27 15:54:26 -0300

    Optmizing MainCoroutineRule

--- app/src/test/java/com/picpay/desafio/android/utils/MainCoroutineRule.kt
+++ app/src/test/java/com/picpay/desafio/android/utils/MainCoroutineRule.kt
@@ -11,18 +11,18 @@ import org.junit.rules.TestWatcher
 import org.junit.runner.Description
 
 @ExperimentalCoroutinesApi
-class MainCoroutineRule (
-    private val dispatcher : CoroutineDispatcher = TestCoroutineDispatcher()
-) : TestWatcher(), TestCoroutineScope by TestCoroutineScope(dispatcher){
+class MainCoroutineRule : TestWatcher(){
 
-    override fun starting(description: Description?) {
+    private val dispatcher = TestCoroutineDispatcher()
+
+    override fun starting(description: Description) {
         super.starting(description)
         Dispatchers.setMain(dispatcher)
     }
 
-    override fun finished(description: Description?) {
+    override fun finished(description: Description) {
         super.finished(description)
-        cleanupTestCoroutines()
+        dispatcher.cleanupTestCoroutines()
         Dispatchers.resetMain()
     }
 }
