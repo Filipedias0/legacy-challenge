276089942a8b6398b0d504ea6d2ed07cf456e44f
Filipedias0 <50221162+Filipedias0@users.noreply.github.com>
2022-10-04 16:36:07 -0300

    Optmizing AppInject

--- app/src/main/java/com/picpay/desafio/android/di/AppInject.kt
+++ app/src/main/java/com/picpay/desafio/android/di/AppInject.kt
@@ -1,19 +1,22 @@
 package com.picpay.desafio.android.di
 
 import androidx.room.Room
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import com.picpay.desafio.android.data.remote.UserService
 import com.picpay.desafio.android.data.db.UserDAO
 import com.picpay.desafio.android.data.db.UserDatabase
 import com.picpay.desafio.android.domain.repository.UserRepository
 import com.picpay.desafio.android.data.repository.UserRepositoryImpl
-import com.picpay.desafio.android.domain.interactors.GetUsersFromRemote
-import com.picpay.desafio.android.domain.interactors.InsertContactListIntoDb
+import com.picpay.desafio.android.domain.interactors.GetUsers
 import com.picpay.desafio.android.presentation.viewModels.MainViewModel
 import com.picpay.desafio.android.util.constants.Constants.USER_DATABASE_NAME
+import okhttp3.Cache
 import okhttp3.OkHttpClient
 import org.koin.android.ext.koin.androidApplication
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.module.Module
+import org.koin.dsl.bind
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -22,25 +25,24 @@ object AppInject {
     private const val BASE_URL = "https://609a908e0f5a13001721b74e.mockapi.io/picpay/api/"
 
     private val viewModelsModule = module {
-        viewModel { MainViewModel(get(), get(), get()) }
+        viewModel { MainViewModel(get()) }
     }
 
     private val networkModule = module{
         single {
-            Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .client(
-                    OkHttpClient.Builder()
-                        .build()
-                )
-                .build()
-                .create(UserService::class.java)
+            okHttpProvider()
+        }
+
+        single {
+            gsonProvider()
+        }
+
+        single {
+            createService<UserService>(get(), get())
         }
     }
 
     private val dataModule = module {
-
 
         single<UserRepository> { UserRepositoryImpl(get(), get()) }
 
@@ -59,8 +61,7 @@ object AppInject {
     }
 
     private val useCasesModule = module {
-        factory<GetUsersFromRemote> { GetUsersFromRemote(get()) }
-        factory<InsertContactListIntoDb> { InsertContactListIntoDb(get()) }
+        factory<GetUsers> { GetUsers(get()) }
     }
 
     fun modules(): List<Module> =
@@ -70,5 +71,23 @@ object AppInject {
             add(viewModelsModule)
             add(networkModule)
         }
+
+    fun okHttpProvider() =
+        OkHttpClient.Builder()
+            .build()
+
+    fun gsonProvider() = GsonBuilder().create()
+
+    private inline fun <reified T> createService(
+        okHttp: OkHttpClient,
+        gson: Gson
+    ): T {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttp)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+            .create(T::class.java)
+    }
 }
 
